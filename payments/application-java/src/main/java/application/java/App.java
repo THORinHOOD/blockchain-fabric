863cb366d8199cd4ffd5a4cc4cfd5cc9f83e9a8d
Asgar Zagitov <ihateyouthor@gmail.com>
2020-11-10 20:19:14 +0300

    first commit

---
+++ payments/application-java/src/main/java/application/java/App.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Running TestApp: 
+// gradle runApp 
+
+package application.java;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import org.hyperledger.fabric.gateway.Contract;
+import org.hyperledger.fabric.gateway.Gateway;
+import org.hyperledger.fabric.gateway.Network;
+import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallets;
+
+
+public class App {
+
+	static {
+		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
+	}
+
+	// helper function for getting connected to the gateway
+	public static Gateway connect() throws Exception{
+		// Load a file system based wallet for managing identities.
+		Path walletPath = Paths.get("wallet");
+		Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+		// load a CCP
+		Path networkConfigPath = Paths.get("..", "..", "test-network", "organizations",
+				"peerOrganizations", "org1.example.com", "connection-org1.yaml");
+		Gateway.Builder builder = Gateway.createBuilder();
+		builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+		return builder.connect();
+	}
+
+	public static void main(String[] args) throws Exception {
+		// enrolls the admin and registers the user
+		try {
+			EnrollAdmin.main(null);
+			RegisterUser.main(null);
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+
+		// connect to the network and invoke the smart contract
+		try (Gateway gateway = connect()) {
+
+			// get the network and contract
+			Network network = gateway.getNetwork("mychannel");
+			Contract contract = network.getContract("payments");
+
+			byte[] result;
+
+
+			result = contract.submitTransaction("addPayment", "12", "111");
+			System.out.println("Evaluate Transaction: addPayment, result: " + new String(result));
+
+			System.out.println("\n");
+			System.out.println("Evaluate Transaction: readPayment");
+			result = contract.evaluateTransaction("readPayment", new Scanner(System.in).nextLine());
+			System.out.println("Evaluate Transaction: readPayment, result: " + new String(result));
+		}
+		catch(Exception e){
+			System.err.println(e);
+		}
+
+	}
+}
